b699a3eeb50c23dc711fa348878115596099b7d5
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-20 22:39:25 +0300

    feat: add delete user by id handling in routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,5 +7,6 @@ router.get('/users', userController.getAllUsers);
 router.get('/users/:userId', userController.getUserById);
 router.post('/users', userController.createUser);
 router.put('/users/:userId', userController.updateUser);
+router.delete('/users/:userId', userController.deleteUser);
 
 export default router;
