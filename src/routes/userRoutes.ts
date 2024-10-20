52ffe9279435cd0e476dbf45b99a54779154f265
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-20 22:29:44 +0300

    feat: add adding user handling in routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import { userController } from '../controllers/userController';
 const router = express.Router();
 
 router.get('/users', userController.getAllUsers);
+router.post('/users', userController.createUser);
 
 export default router;
