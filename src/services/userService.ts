5be8eb7e3ccd8161a1053349dc57979327009451
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-20 22:30:35 +0300

    feat: add creating user handling in services

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
 import { User } from '../models/userModel';
 import { db } from '../db/inMemoryDb';
 
 export const userService = {
   getAllUsers: (): User[] => db.getUsers(),
+
+  createUser: (username: string, age: number, hobbies: string[]): User => {
+    const newUser: User = {
+      id: uuidv4(),
+      username,
+      age,
+      hobbies,
+    };
+    db.addUser(newUser);
+    return newUser;
+  },
+
 };
