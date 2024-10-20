f47abd7a6fcbf59dee526b78279b8422871aac59
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-20 22:34:39 +0300

    feat: add get user by id handling in db

--- src/db/inMemoryDb.ts
+++ src/db/inMemoryDb.ts
@@ -4,6 +4,7 @@ const users: User[] = [];
 
 export const db = {
   getUsers: (): User[] => users,
+  getUserById: (id: string): User | undefined => users.find(user => user.id === id),
   addUser: (user: User): void => {
     users.push(user);
   },
